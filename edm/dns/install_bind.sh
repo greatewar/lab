3d9ef0068a90ad4e06f2ae5cf42b9b2baef8d369
greatewar <greatewar@gmail.com>
2023-05-21 15:33:17 +0800

    update

--- edm/dns/install_bind.sh
+++ edm/dns/install_bind.sh
@@ -9,16 +9,16 @@ sudo yum install bind bind-utils -y
 # 配置 BIND
 sudo bash -c "cat > /etc/named.conf << EOL
 options {
-    directory   "/var/named";
-    dump-file   "/var/named/data/cache_dump.db";
-    statistics-file "/var/named/data/named_stats.txt";
-    memstatistics-file "/var/named/data/named_mem_stats.txt";
+    directory   \"/var/named\";
+    dump-file   \"/var/named/data/cache_dump.db\";
+    statistics-file \"/var/named/data/named_stats.txt\";
+    memstatistics-file \"/var/named/data/named_mem_stats.txt\";
     dnssec-enable yes;
     dnssec-validation yes;
-    bindkeys-file "/etc/named.iscdlv.key";
-    managed-keys-directory "/var/named/dynamic";
-    pid-file "/run/named/named.pid";
-    session-keyfile "/run/named/session.key";
+    bindkeys-file \"/etc/named.iscdlv.key\";
+    managed-keys-directory \"/var/named/dynamic\";
+    pid-file \"/run/named/named.pid\";
+    session-keyfile \"/run/named/session.key\";
     listen-on port 53 { any; };
     allow-query     { any; };
     recursion yes;
@@ -26,15 +26,15 @@ options {
 };
 logging {
     channel default_debug {
-        file "data/named.run";
+        file \"data/named.run\";
         severity dynamic;
     };
 };
-zone "." IN {
+zone \".\" IN {
     type hint;
-    file "named.ca";
+    file \"named.ca\";
 };
-include "/etc/named.rfc1912.zones";
+include \"/etc/named.rfc1912.zones\";
 EOL"
 
 # 重启 BIND 服务
